8bb38338d7727631c8a1cbbad7c511e110b3d3e4
Dell-User <Dell-User@DESKTOP-4LNHG9G>
2023-10-09 20:23:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16.While_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 0;
+            Console.WriteLine(index);
+            index++;
+
+            Console.WriteLine("This is the do while loop");
+            int index2 = 0;
+            do
+            {
+                Console.WriteLine(index2);
+                index2++;
+            }
+            while (index2 <= 5);
+            //freeze
+            Console.Read();
+
+
+        }
+    }
+}
